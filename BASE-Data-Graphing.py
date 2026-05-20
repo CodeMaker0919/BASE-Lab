da468a2c3e22045773ba80cfd7c5c3e8627c2951
CodeMaker0919 <nallakukkala.school@gmail.com>
2026-05-20 13:14:09 -0400

    Update BASE-Data-Graphing.py

--- BASE-Data-Graphing.py
+++ BASE-Data-Graphing.py
@@ -46,8 +46,10 @@ def build_precision_graph(data, y_cols, title):
     label_positions = []
 
     for group_name in data['Group'].unique():
+        for group_name in data['Group'].unique():
         gdf = data[data['Group'] == group_name].sort_values('Date')
         plot_data = gdf.copy()
+        plot_data['IsReal'] = True 
 
         # --- GHOST POINT INTERPOLATION ---
         for i in range(len(gdf) - 1):
